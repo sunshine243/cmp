da079374ad41f746ce773e325997a8133d3458ee
sunshine243 <yaukimhau@gmail.com>
2024-04-22 20:31:13 +0800

    init

--- login.php
+++ login.php
@@ -1,6 +1,30 @@
 <?php
-session_start();
+include ('auth.php');
+require('database.php');
+if (isset($_POST['submit'])){
+    $username = stripslashes($_POST['username']);
+    $username = mysqli_real_escape_string($con,$username);
+    $password = stripslashes($_POST['password']);
+    $password = mysqli_real_escape_string($con,$password);
+    $password = md5($password); // Hash the password
+    $query = "SELECT * FROM `admin` WHERE username='$username' AND password='$password'";
+    $result = mysqli_query($con,$query) or die(mysqli_error($con));
+    $rows = mysqli_num_rows($result);
+
+    if($rows==1){
+        $_SESSION['username'] = $username;
+        echo "<script type='text/javascript'>window.location.href = 'index.php';</script>";
+
+        exit();
+    } else {
+        echo "<div class='login-box'>
+                <h3>Username/password is incorrect.</h3>
+                <br/>Click here to <a href='login.php'>Login</a>
+              </div>";
+    }
+}
 ?>
+
 <!DOCTYPE html>
 <html>
 <head>
@@ -9,59 +33,31 @@ session_start();
 <title>Admin Login</title>
 </head>
 <body>
-<?php
-require('database.php');
-if (isset($_POST['submit'])){
-$username = stripslashes($_POST['username']);
-$username = mysqli_real_escape_string($con,$username);
-$password = stripslashes($_POST['password']);
-$password = mysqli_real_escape_string($con,$password);
- $query = "SELECT *
- FROM `admin`
- WHERE username='$username'
- AND password='".md5($password)."'"
- ;
-$result = mysqli_query($con,$query) or die(mysqli_error($con));
-$rows = mysqli_num_rows($result);
- if($rows==1){
- $_SESSION['username'] = $username;
- echo "<script> alert(" + $username + "）</script>";
- header("Location: index.php");
- exit();
- }else{
-echo "<div class='login-box'>
-<h3>Username/password is incorrect.</h3>
-<br/>Click here to <a href='login.php'>Login</a></div>";
-}
- }else{
-?>
-<form method = "POST" class="login-box">
-        <div class="login-header">
-            <header>Login</header>
-        </div>
-        <div class="input-box">
-            <input type="text" class="input-field" placeholder="Email" autocomplete="off" required>
-        </div>
-        <div class="input-box">
-            <input type="password" class="input-field" placeholder="Password" autocomplete="off" required>
-        </div>
-        <div class="forgot">
-            <section>
-                <input type="checkbox" id="check">
-                <label for="check">Remember me</label>
-            </section>
-            <section>
-                <a href="#">Forgot password</a>
-            </section>
-        </div>
-        <div class="input-submit">
-            <a href="index.php" class="submit-btn" name = "submit">Sign In</a>
-        </div>
-        <div class="sign-up-link">
-            <p>Don't have account? <a href="registration.php">Sign Up</a></p>
-        </div>
- </form>
-    <?php } ?>
+<form method="POST" class="login-box">
+    <div class="login-header">
+        <header>Login</header>
+    </div>
+    <div class="input-box">
+        <input type="text" class="input-field" placeholder="Email" autocomplete="off" name="username" required>
+    </div>
+    <div class="input-box">
+        <input type="password" class="input-field" placeholder="Password" autocomplete="off" name="password" required>
+    </div>
+    <div class="forgot">
+        <section>
+            <input type="checkbox" id="check">
+            <label for="check">Remember me</label>
+        </section>
+        <section>
+            <a href="#">Forgot password</a>
+        </section>
+    </div>
+    <div class="input-submit">
+        <input type="submit" class="submit-btn" name="submit" value="Sign In">
+    </div>
+    <div class="sign-up-link">
+        <p>Don't have an account? <a href="registration.php">Sign Up</a></p>
+    </div>
+</form>
 </body>
 </html>
-
